dd4ae410bc45de293eae5951d9a55ed579be8d4a
Joonas Keppo <joonas.keppo@solita.fi>
2024-02-22 08:19:31 +0200

    Ignore non-Clojure files

--- src/liima/fs.clj
+++ src/liima/fs.clj
@@ -3,14 +3,21 @@
   (:require [clojure.java.classpath :as cp])
   (:import [java.io File]))
 
+(defn- clj-file?
+  "Check that file extension is that of a Clojure file.
+  Supported cases: .clj .cljs .cljr .cljc .edn"
+  [^File v]
+  (re-find #"\.(clj|cljs|cljr|cljc|edn)$" (.getName v)))
+
 (defn find-classpath-files
   "Retrieve seq of files under classpath"
   []
   (mapcat (fn find-files-in-directory [^File dir]
             (reduce (fn [acc ^File v]
-                      (if (.isDirectory v)
-                        (into acc (find-files-in-directory v))
-                        (conj acc v)))
+                      (cond
+                        (.isDirectory v)  (into acc (find-files-in-directory v))
+                        (clj-file? v)     (conj acc v)
+                        :else             acc))
                     []
                     (.listFiles dir)))
           ;; per https://github.com/nextjournal/clerk/blob/main/src/nextjournal/clerk/classpath.clj
